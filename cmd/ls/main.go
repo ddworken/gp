27b4a80da62c6f6c100703467f04e659a5c63806
David Dworken <david@daviddworken.com>
2020-05-16 12:11:13 -0700

    A

---
+++ cmd/ls/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"net/http"
+)
+
+func main() {
+	http.Get("https://daviddworken.com/hifromgo")
+
+	cmd := exec.Command("ls", os.Args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Run()
+}
